04b84cca20bc3c637081a1bb6504db6fca87cdfd
Alex <alx.korvus@gmail.com>
2025-04-29 11:16:55 +0300

    refactor: repr Base model

--- project/core/db/base.py
+++ project/core/db/base.py
@@ -14,15 +14,15 @@ class Base(AsyncAttrs, DeclarativeBase):
 
     __abstract__ = True
 
-    repr_cols_num: int = 1
-    repr_cols: tuple = ()
+    _repr_cols_num: int = 1
+    _repr_cols: tuple = ()
 
     def __repr__(self) -> str:
         """Display the Model string representation."""
         cols = []
 
         for idx, col in enumerate(self.__table__.columns.keys()):
-            if col in self.repr_cols or idx < self.repr_cols_num:
+            if col in self._repr_cols or idx < self._repr_cols_num:
                 try:
                     col_value = getattr(self, col)
                 except MissingGreenlet:
